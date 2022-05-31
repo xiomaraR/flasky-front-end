53e5edbb03e4c20d66fda175888fc1fc71635b56
Becca Elenzil <becca@adadevelopersacademy.org>
2022-05-31 09:02:04 -0700

    add createChip behavior and delete dog button

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,14 +1,31 @@
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./Dog.css";
 
 const Dog = (props) => {
+  const [chipNum, setChipNum] = useState(props.chip);
+
+  //https://www.w3schools.com/js/js_random.asp
+  const getRndInteger = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  const createChip = () => {
+    setChipNum(getRndInteger(1000, 9999));
+    console.log();
+  };
+
   return (
     <li>
-      <h2>{props.name}</h2>
-      {props.chip ? (
-        <span>Chip #: {props.chip}</span>
+      <h2>
+        {props.name}
+        <button className="delete__dog">delete</button>
+      </h2>
+
+      {chipNum ? (
+        <span>Chip #: {chipNum}</span>
       ) : (
-        <button>add chip</button>
+        <button onClick={createChip}>add chip</button>
       )}
     </li>
   );
